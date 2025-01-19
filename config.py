00ec0adc3123e695633fdd125a24a558b6f99db3
kucingcoklat512 <123432009+kucingcoklat512@users.noreply.github.com>
2025-01-20 03:59:09 +0900

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,7 @@ app = Flask(__name__)
 CORS(app, resources={"/api/*": {"origins": "*"}}, supports_credentials=True)
 
 # Konfigurasi MySQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://mcw_01:mcw123@localhost:3306/web2_db'
-# mysql+mysqlconnector://apigames_itstypehim:29b0b9e4dc840a16808d694ac2a2203e813bc7b7@z0f3t.h.filess.io:3305/apigames_itstypehim
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://apigames2_diameterat:9c054e953669335c46c7ec3589c53a29199598b7@localhost:3305/apigames2_diameterat'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'test123'
 db = SQLAlchemy(app)
