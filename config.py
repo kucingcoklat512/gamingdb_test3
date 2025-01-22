ef016a02a8e4fb846d198e132d09cb8a9c0852a1
kucingcoklat512 <123432009+kucingcoklat512@users.noreply.github.com>
2025-01-22 21:20:20 +0900

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 
 # Initialize app and database connection
 app = Flask(__name__)
-CORS(app, resources={"/api/*": {"origins": "*"}}, supports_credentials=True)
+CORS(app, resources={"/api/*": {"origins": "https://gamingdb-test4.vercel.app/"}}, supports_credentials=True)
 
 # Konfigurasi MySQL
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://apigames2comp2_colonyhard:999e8fc3a619e3587ce746e51507f4d942a31e83@98a82.h.filess.io:3305/apigames2comp2_colonyhard'
