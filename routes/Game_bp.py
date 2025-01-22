cedae46eac115029a38cf3d41160407145a92201
kucingcoklat512 <123432009+kucingcoklat512@users.noreply.github.com>
2025-01-22 22:07:53 +0900

    Update Game_bp.py

--- routes/Game_bp.py
+++ routes/Game_bp.py
@@ -1,7 +1,9 @@
+from flask_cors import CORS
 from flask import Blueprint
 from controllers.GameController import get_games, get_game, add_game, update_game, patch_game, delete_game
 
 game_bp = Blueprint('game_bp', __name__)
+CORS(game_bp, resources={r"/api/games*": {"origins": "https://gamingdb-test4.vercel.app/"}}, supports_credentials=True)
 
 game_bp.route('/api/games', methods=['GET'])(get_games)
 game_bp.route('/api/games/<int:game_id>', methods=['GET'])(get_game)
@@ -9,7 +11,6 @@ game_bp.route('/api/games', methods=['POST'])(add_game)
 game_bp.route('/api/games/<int:game_id>', methods=['PUT'])(update_game)
 game_bp.route('/api/games/<int:game_id>', methods=['PATCH'])(patch_game)
 game_bp.route('/api/games/<int:game_id>', methods=['DELETE'])(delete_game)
-
 
 # from flask import Blueprint
 # from flask_restx import Api, Resource, fields
