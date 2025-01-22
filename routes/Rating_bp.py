96c1f5b3303d83932b54380661c7dc60616b0930
kucingcoklat512 <123432009+kucingcoklat512@users.noreply.github.com>
2025-01-22 21:53:55 +0900

    Update Rating_bp.py

--- routes/Rating_bp.py
+++ routes/Rating_bp.py
@@ -2,10 +2,10 @@ from flask import Blueprint
 from controllers.RatingController import get_ratings, get_rating, add_rating, update_rating, delete_rating
 from flask_cors import CORS
 
-CORS(blueprint_instance, resources={r"/*": {"origins": "https:/https://gamingdb-test4.vercel.app/"}}, supports_credentials=True)
+
 
 rating_bp = Blueprint('rating_bp', __name__)
-
+CORS(blueprint_instance, resources={r"/*": {"origins": "https:/https://gamingdb-test4.vercel.app/"}}, supports_credentials=True)
 rating_bp.route('/api/ratings', methods=['GET'])(get_ratings)
 rating_bp.route('/api/ratings/<int:id_rate>', methods=['GET'])(get_rating)
 rating_bp.route('/api/ratings', methods=['POST'])(add_rating)
